7659416309f161871438a9904aa5d3ecadd0974e
fr0meupp <haydarrhouma3@gmail.com>
2026-05-09 00:16:16 +0200

    Remove Android app

--- android-app/android/app/src/main/java/money/twocents/client/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package money.twocents.client;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- android-app/src/components/onboarding/constants.ts
+++
@@ -1,1 +0,0 @@
-export const ONBOARDING_KEY = '2c_onboarding_done'
